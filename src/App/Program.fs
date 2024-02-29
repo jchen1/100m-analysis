781eddb82ba1be316a1b6239ebf48861abc06c11
Jeff Chen <hello@jeff.yt>
2024-02-29 11:34:34 -0700

    add nonexistent reaction times

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -11,7 +11,7 @@ type LineType =
 
 type RaceData =
     { Athlete: string
-      Reaction: decimal
+      Reaction: decimal option
       SplitData: decimal list
       Time: decimal }
 
@@ -19,8 +19,8 @@ type ParseState =
     { LastLine: LineType
       RaceData: RaceData list }
 
-let cumulativeSplitDataRegex = Regex @"(?<LastName>[\p{L}-]+),? (?<FirstName>[\p{L}-]+) .*time(?<Splits>(?: [\d\.]+){10})"
-let splitDataRegex = Regex @"reaction time (?<ReactionTime>[\d\.]+) interval(?: [\d\.]+){9}"
+let cumulativeSplitDataRegex = Regex @"(?<LastName>[\p{L}- ]+),? (?<FirstName>[\p{L}- ]+) .*time(?<Splits>(?: [\d\.]+){10})"
+let splitDataRegex = Regex @"reaction time (?<ReactionTime>[\d\.]+)?\s*interval(?: [\d\.]+){9}"
 let parseRaceData (CumulativeSplitData line1) (SplitData line2) : Option<RaceData> =
     let match1 = cumulativeSplitDataRegex.Match line1
     let match2 = splitDataRegex.Match line2
@@ -28,9 +28,12 @@ let parseRaceData (CumulativeSplitData line1) (SplitData line2) : Option<RaceDat
     match match1.Success, match2.Success with
     | true, true ->
         let splits = match1.Groups.["Splits"].Value.Trim().Split(' ') |> Array.map decimal
-        let reaction = decimal match2.Groups.["ReactionTime"].Value
+        let reaction =
+            match match2.Groups.["ReactionTime"].Value with
+            | "" -> None
+            | reaction -> Some (decimal reaction)
         
-        let splitData = (Seq.append (Seq.singleton reaction) splits) |> Seq.pairwise |> Seq.map (fun (a, b) -> b - a) |> Seq.toList
+        let splitData = (Seq.append (Seq.singleton (Option.defaultValue 0m reaction)) splits) |> Seq.pairwise |> Seq.map (fun (a, b) -> b - a) |> Seq.toList
         let firstName = match1.Groups.["FirstName"].Value
         let lastName = match1.Groups.["LastName"].Value
         
@@ -52,18 +55,19 @@ let parseLine (state: ParseState) (rawLine: string) =
         | CumulativeSplitData _, SplitData _ -> parseRaceData state.LastLine line
         | _ -> None
     
-    // match state.LastLine, line with
-    // | _, Ignored l when Regex.IsMatch(l, "reaction time [\d\.]+") -> printfn $"DID NOT PARSE: {l}"
-    // | Ignored l, SplitData _ -> printfn $"DID NOT PARSE: {l}"
-    // | _, _ -> ()
-    
     { LastLine = line; RaceData = match raceData with Some data -> data :: state.RaceData | None -> state.RaceData } 
 
 let lines = System.IO.File.ReadLines("data/M100m-by-meeting-Sep-23.txt")
 let result = lines |> Seq.fold parseLine { LastLine = Ignored ""; RaceData = [] }
 
-let printLine (r: RaceData) =
+let toCsvLine (r: RaceData) =
     let splits = String.concat ", " (r.SplitData |> List.map string)
-    printfn $"{r.Athlete}, {r.Reaction}, {r.Time}, {splits}"
+    $"{r.Athlete}, {Option.defaultValue System.String.Empty (Option.map (_.ToString()) r.Reaction)}, {r.Time}, {splits}"
     
-result.RaceData |> List.iter printLine
+let csvData = result.RaceData |> List.map toCsvLine |> String.concat "\n"
+let csvHeader = "Athlete, Reaction, Time, 10m, 20m, 30m, 40m, 50m, 60m, 70m, 80m, 90m, 100m"
+let csv = $"{csvHeader}\n{csvData}"
+
+System.IO.File.WriteAllText("/Users/jeff/Documents/Projects/100m-analysis/data/parsed.csv", csv)
+
+printfn $"Parsed {result.RaceData.Length} records. Wrote to data/parsed.csv."
